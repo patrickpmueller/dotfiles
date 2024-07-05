a90840193fac5aaf6fb05aee98c35ad251855372
Patrick Pellacani Müller <patrickpmueller@yahoo.com>
2024-07-05 20:55:40 +0100

    focus mode script fix

--- .config/hypr/focus_mode.sh
+++ .config/hypr/focus_mode.sh
@@ -3,11 +3,13 @@
 LOCK_FILE='/home/patrickpmueller/.cache/focus.lock'
 
 if [[ ! -f "$LOCK_FILE" ]]; then
-	hyprctl keyword decoration:active_opacity 0.85
-	hyprctl keyword decoration:inactive_opacity 0.85
+	hyprctl keyword decoration:active_opacity 0.8
+	hyprctl keyword decoration:inactive_opacity 0.8
+	hyprctl keyword decoration:blur:enabled true
 	touch "$LOCK_FILE"
 else
 	hyprctl keyword decoration:active_opacity 1
 	hyprctl keyword decoration:inactive_opacity 1
+	hyprctl keyword decoration:blur:enabled false
 	rm "$LOCK_FILE"
 fi
