fb6089a64c6fbd2e914898414c01d15bc0ac75ac
Patrick Pellacani Müller <patrickpmueller@yahoo.com>
2024-07-06 10:32:36 +0000

    updates

--- .config/hypr/focus_mode.sh
+++ .config/hypr/focus_mode.sh
@@ -3,8 +3,8 @@
 LOCK_FILE='/home/patrickpmueller/.cache/focus.lock'
 
 if [[ ! -f "$LOCK_FILE" ]]; then
-	hyprctl keyword decoration:active_opacity 0.85
-	hyprctl keyword decoration:inactive_opacity 0.85
+	hyprctl keyword decoration:active_opacity 0.88
+	hyprctl keyword decoration:inactive_opacity 0.88
 	touch "$LOCK_FILE"
 else
 	hyprctl keyword decoration:active_opacity 1
