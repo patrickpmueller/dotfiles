5857b532a13ff1fca34e1dcb51f5a6f8a8884e4d
Patrick Pellacani Müller <patrickpmueller@yahoo.com>
2024-07-28 19:03:59 +0000

    Update config

--- .config/eww/dashboard/toggle.sh
+++ .config/eww/dashboard/toggle.sh
@@ -4,7 +4,7 @@ LOCK_FILE="$HOME/.cache/dashboard.lock"
 
 if [[ ! -f "$LOCK_FILE" ]]; then
     touch "$LOCK_FILE"
-    eww -c $HOME/.config/eww/dashboard open dashboard 
+   	eww -c $HOME/.config/eww/dashboard open dashboard 
 else
     eww -c $HOME/.config/eww/dashboard close dashboard
     rm "$LOCK_FILE"
